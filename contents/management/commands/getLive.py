cdcdbe50b8344a18f76254d187a5017f27e2f063
ynj0906 <n.yasuhito@gmail.com>
2019-11-14 17:15:54 +0900

    a

---
+++ contents/management/commands/getLive.py
@@ -0,0 +1,86 @@
+# coding: UTF-8
+import requests, os, re,sys
+from bs4 import BeautifulSoup
+from datetime import date, datetime
+import pathlib
+import pytz
+from django.core.management import BaseCommand
+from linebot.models import MessageEvent, TextMessage, FollowEvent, UnfollowEvent, TextSendMessage, ImageMessage, \
+    AudioMessage
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import LineBotApiError
+from dotenv import load_dotenv
+
+from contents.management.commands import usersDB
+
+
+class Command(BaseCommand):
+    
+    
+    def tweetinfo(self):
+        url = "https://twitter.com/rev84"
+        ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/55.0.2883.95 Safari/537.36 '
+        req = requests.get(url, headers={'User-Agent': ua})
+        soup = BeautifulSoup(req.content, "html.parser")
+        # find_allでdiv>content全て取得
+        all_contents = soup.find_all('div', attrs={'class': 'content'})
+
+        for i in all_contents:
+            topicsindex = i.find('small', attrs={'class': 'time'})
+            unixtime = int(topicsindex.find('span').get('data-time'))
+            today_unixtime = datetime.fromtimestamp(unixtime, tz=pytz.timezone('Asia/Tokyo')).replace(hour=0, minute=0,
+                                                                                                      second=0,
+                                                                                                      microsecond=0)
+            # 今日の日付をタイムゾーン付きの年月日のみで取得
+            today_tzone = datetime.now(pytz.timezone('Asia/Tokyo')).replace(hour=0, minute=0, second=0, microsecond=0)
+
+            # ツイート本文を取得
+            topicsindex2 = i.find('p', attrs={'class': 'TweetTextSize'})
+
+            # twitterの投稿日が今日 & 対象の言葉が含まれてたら、テキスト本文取得
+            if today_unixtime == today_tzone and "配信を開始しました" in topicsindex2.text:
+                with open("contents/management/commands/aaa.txt", "r+", encoding="utf-8", newline=None)as f:
+                    # 対象ツイートのunixtimeを以て、一度実行済みならスルー
+                    if str(unixtime) == f.read().rstrip("\n"):
+                        print("一回スクレイピング済み")
+                        break
+                    elif str(unixtime) != f.read().rstrip("\n"):
+                        # ファイルをr+で開いていると追記になるので、w+で開き直して上書きの上、unixtimeを追加
+                        with open("contents/management/commands/aaa.txt", "w+", encoding="utf-8", newline='\n')as ff:
+                            ff.write(str(unixtime) + '\n')
+                            print("はじまった")
+                            return True
+                    else:
+                        print("それ以外")
+                        pass
+            else:
+                print("該当なし")
+                pass
+
+    def handle(self,*args, **options):
+        from socket import gethostname
+        from os import environ
+
+        HOSTNAME = gethostname()
+
+        # if 'DESKTOP' in HOSTNAME:
+        #     current_dir = pathlib.Path(__file__).parents[3].joinpath('.env')
+        #     # sys.path.append(str(current_dir) + '/..../')
+        #     from config import local_settings
+        #     channel_access_token = local_settings.LINE_CHANNEL_ACCESS_TOKEN
+        # else:
+        #     channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+        current_dir11 = pathlib.Path(__file__).parents[3].joinpath('config', '.env')
+        load_dotenv(str(current_dir11))
+        channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+        
+        
+
+        if self.tweetinfo():
+            line_bot_api = LineBotApi(channel_access_token)
+            try:
+                line_bot_api.push_message("Uc148172028f01d4635bdb232e6b00920", TextSendMessage(text="はじまったで！"))
+            except LineBotApiError as e:
+                return e
---
+++ contents/views.py
@@ -0,0 +1,89 @@
+from django.http import HttpResponseForbidden, HttpResponse
+from django.shortcuts import render
+import os, sys, requests, csv
+from django.http import HttpResponseForbidden, HttpResponse
+
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent, TextMessage, FollowEvent, UnfollowEvent,
+    TextSendMessage, ImageMessage, AudioMessage
+)
+
+from linebot import LineBotApi, WebhookHandler
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+line_channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(line_channel_secret)
+
+
+# Create your views here.
+
+# 確認用view
+def index(request):
+    return HttpResponse(123)
+
+
+
+#
+# @handler.add(FollowEvent)
+# def handle_follow(event):
+#     """
+#     友だち追加したときのイベント。
+#     UsersDBにIDと性別、アクティビティを追加。
+#     """
+#     UserID = event.source.user_id
+#     users_DB.add(UserID)
+#     buttons_template = ButtonsTemplate(
+#         title='友達追加ありがとう！', text='まず、あなたの性別を教えてください!', actions=[
+#             PostbackAction(label='男', data='male'),
+#             PostbackAction(label='女', data='female'),
+#         ])
+#     template_message = TemplateSendMessage(alt_text='友達追加ありがとう！\nまず、あなたの性別を教えてください。', template=buttons_template)
+#     line_bot_api.reply_message(event.reply_token, template_message)
+#
+# @handler.add(UnfollowEvent)
+# def handle_unfollow(event):
+#     """
+#     ブロックされた時のイベント。
+#     UsersDBのIDと性別、アクティビティを削除。
+#     ＜TODO＞　QuestionDBの投稿を削除。
+#     """
+#     UserID = event.source.user_id
+#     users_DB.remove(UserID)
+#
+
+@csrf_exempt
+def callback(request):
+    # signatureの取得
+    signature = request.META['HTTP_X_LINE_SIGNATURE']
+    body = request.body.decode('utf-8')
+    try:
+        # 署名の検証を行い、成功した場合にhandleされたメソッドを呼び出す
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return HttpResponseForbidden()
+    return HttpResponse('OK')
+
+
+# フォローイベントの場合の処理
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='初めまして')
+    )
+#
+#
+# メッセージイベントの場合の処理
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    # メッセージでもテキストの場合はオウム返しする
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="わっふる、わっふる")
+    )
